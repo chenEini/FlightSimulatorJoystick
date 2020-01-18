5364d33ab96a42a694a970863d9f8d9ef627703e
chenEini <chen.eini000@gmail.com>
2020-01-18 21:56:17 +0200

    add connection popup to FlightGear

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -1,12 +1,22 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import view_model.ViewModel;
 
 public class MainWindowController {
 
 	ViewModel vm;
+	
+	@FXML
+	Button openConnectPopup;
+	
+	@FXML
+	TextField connectionIp;
+	@FXML
+	TextField connectionPort;
 
 	@FXML
 	Slider throttle;
